9ae2bc0bfd96959c4186dac3f71ff40838dbf8ff
Prabhjyot Singh <prabhjyotsingh2123@gmail.com>
2026-03-25 12:39:25 +0530

    Project setup with env variables

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,25 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC98XpbsmcSbZBSafNiGVHw1aoaoYNWJVU",
-  authDomain: "microlearning-app-962b3.firebaseapp.com",
-  projectId: "microlearning-app-962b3",
-  storageBucket: "microlearning-app-962b3.firebasestorage.app",
-  messagingSenderId: "586618784804",
-  appId: "1:586618784804:web:67c100a729064999c2dcb1",
-  measurementId: "G-WEMM0FX7HG"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db }; // ✅ named export
+export const db = getFirestore(app);
 export const auth = getAuth(app);
